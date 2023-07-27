6070d8f23878a5ed90302b93bf2a2c74a0ee0599
NehaShinde01 <116095704+NehaShinde01@users.noreply.github.com>
2023-07-27 08:13:30 +0000

    create components and services of user and todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TodosComponent } from './todos/todos.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
+  { path: 'dashborard', component: DashboardComponent },
+  { path: 'todo', component: TodosComponent },
+  { path: 'users', component: UsersComponent },
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule, DashboardComponent, TodosComponent, UsersComponent]
 })
 export class AppRoutingModule { }
